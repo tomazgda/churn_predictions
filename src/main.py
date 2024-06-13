7e4cb11c6e781ca93987f52d1f48bc230f5f7ee5
tomazgda <36647682+tomazgda@users.noreply.github.com>
2024-06-13 14:21:11 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 # read csv into dataframe
-data = pd.read_csv("../telco_data.csv")
+data = pd.read_csv("telco_data.csv")
 
 # write the first 10 rows of the dataframe into a new csv file
-pd.head(10).to_csv("../telo_data_head.csv")
+pd.head(10).to_csv("telo_data_head.csv")
 
 print("Run Successful!\n")
