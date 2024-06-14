b6e09209731e4cc07d0072a720794814de3b97f3
tomazgda <36647682+tomazgda@users.noreply.github.com>
2024-06-14 09:50:16 +0100

    abstracted data splitting to utils clean_and_split function

--- src/main.py
+++ src/main.py
@@ -1,33 +1,20 @@
 import numpy as np
 import pandas as pd
 
+from xgboost import XGBClassifier
+
 # import preprocessor defined in preprocessor.py
-from preprocessor import *
+from preprocessor import Preprocessor
 
-# read csv into dataframe
-data = pd.read_csv("telco_data.csv")
+# import clean_and_split defined in utils.py
+from utils import clean_and_split
 
-# write the first 10 rows of the dataframe into a new csv file
-data.head(10).to_csv("telco_data_head.csv")
-
-# remove rows with missing targets
-data_without_na = data.dropna(axis=0, subset=['Churn'], inplace=False)
-
-# seperate target from features, and encode target data
-y = np.where(data_without_na['Churn'] == 'Yes', 1, 0)
-
-# drop target from whole data set, return features which will be called X
-X = data_without_na.drop(['Churn'], axis=1, inplace=False)[["tenure", "MonthlyCharges", "Contract"]]
-
-# seperature training from validation data
-from sklearn.model_selection import train_test_split 
-
-train_X, valid_X, train_y, valid_y = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
+# define training and validation dataframes 
+train_X, valid_X, train_y, valid_y = clean_and_split("telco_data.csv")
 
 # setup preprocessor
 preprocessor = Preprocessor(train_X)
 
-from xgboost import XGBClassifier
 xgb_model = XGBClassifier(n_estimators=100)
 
 # define the pipeline
---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+def clean_and_split(filename: str) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """reads a csv into a dataframe, removes rows with na targets, and splits data"""
+    # read csv into dataframe
+    data = pd.read_csv(filename)
+
+    # write the first 10 rows of the dataframe into a new csv file
+    data.head(10).to_csv("telco_data_head.csv")
+
+    # remove rows with missing targets
+    data_without_na = data.dropna(axis=0, subset=['Churn'], inplace=False)
+
+    # seperate target from features, and encode target data
+    y = np.where(data_without_na['Churn'] == 'Yes', 1, 0)
+
+    # drop target from whole data set, return features which will be called X
+    X = data_without_na.drop(['Churn'], axis=1, inplace=False)[["tenure", "MonthlyCharges", "Contract"]]
+
+    # seperature training from validation data
+    return train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=0)
+
