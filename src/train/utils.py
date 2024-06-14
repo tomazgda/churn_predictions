b11675da08f037c5cf7fad866411dbe091324884
tomazgda <36647682+tomazgda@users.noreply.github.com>
2024-06-14 15:13:54 +0100

    sync

--- src/train/utils.py
+++ src/train/utils.py
@@ -5,11 +5,9 @@ from sklearn.model_selection import train_test_split
 
 def clean_and_split(filename: str, features: list[str], test_size: float) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """reads a csv into a dataframe, removes rows with na targets, and splits data"""
+    
     # read csv into dataframe
     data = pd.read_csv(filename)
-
-    # write the first 10 rows of the dataframe into a new csv file
-    data.head(10).to_csv("telco_data_head.csv")
 
     # remove rows with missing targets
     data_without_na = data.dropna(axis=0, subset=['Churn'], inplace=False)
@@ -17,7 +15,7 @@ def clean_and_split(filename: str, features: list[str], test_size: float) -> tup
     # seperate target from features, and encode target data
     y = np.where(data_without_na['Churn'] == 'Yes', 1, 0)
 
-    # drop target from whole data set, return features which will be called X
+    # drop target from whole dataset, return features which will be called X
     X = data_without_na.drop(['Churn'], axis=1, inplace=False)[features]
 
     # seperature training from validation data
